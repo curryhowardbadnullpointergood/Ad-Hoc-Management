280c002c38306bdf66fdf6f861a2e16b412e4e08
Ash <darkovl10@gmail.com>
2024-03-24 03:35:28 +0000

    Added new users scene, and a button on the dasboard that let's you switch to this scene

--- seg/src/main/java/soton/ac/uk/seg/scenes/DashboardController.java
+++ seg/src/main/java/soton/ac/uk/seg/scenes/DashboardController.java
@@ -1,6 +1,13 @@
 package soton.ac.uk.seg.scenes;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -8,7 +15,10 @@ import javafx.stage.Stage;
 
 public class DashboardController {
 
-
+    
+    private Stage stage;
+    private Scene scene; 
+    private Parent root; 
 
   @FXML
   private TextField totalcost;
@@ -54,7 +64,15 @@ public class DashboardController {
 
 
 
+  public void switchtoNewUsers(ActionEvent event) throws IOException{
 
+    root = FXMLLoader.load(getClass().getResource("newusers.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+    
+  }
 
 
   
---
+++ seg/src/main/java/soton/ac/uk/seg/scenes/NewusersController.java
@@ -0,0 +1,5 @@
+package soton.ac.uk.seg.scenes;
+
+public class NewusersController {
+    
+}
