66fb833afc2a18b9379cca2497fb6787c9a839ee
Ash <darkovl10@gmail.com>
2024-05-01 17:26:25 +0100

    added some sql

--- seg/src/main/java/soton/ac/uk/seg/scenes/MetricsController.java
+++ seg/src/main/java/soton/ac/uk/seg/scenes/MetricsController.java
@@ -18,6 +18,38 @@ public class MetricsController {
     private Scene scene; 
     private Parent root; 
 
+    @FXML
+    private TextField totalcost;
+
+    @FXML
+    private TextField numofimpressions;
+
+    @FXML
+    private TextField numofuniques;
+
+    @FXML
+    private TextField numofclicks;
+
+    @FXML
+    private TextField numofbounces;
+
+    @FXML
+    private TextField numofconversions;
+
+    @FXML
+    private TextField ctr;
+
+    @FXML
+    private TextField cpa;
+
+    @FXML
+    private TextField cpc;
+
+    @FXML
+    private TextField cpm;
+
+    @FXML
+    private TextField bouncerate;
 
   
 
@@ -30,6 +62,90 @@ public class MetricsController {
     stage.show();
     
   }
+
+  @FXML
+  private void displayImpressions(){
+     int number = 42; 
+     numofimpressions.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displayUniques(){
+     int number = 42; 
+     numofuniques.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displayclicks(){
+     int number = 42; 
+     numofclicks.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaybounces(){
+     int number = 42; 
+     numofbounces.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displayconversions(){
+     int number = 42; 
+     numofconversions.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displayctr(){
+     int number = 42; 
+     ctr.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaycpa(){
+     int number = 42; 
+     cpa.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaycpc(){
+     int number = 42; 
+     cpc.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaycpm(){
+     int number = 42; 
+     cpm.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaybouncerate(){
+     int number = 42; 
+     bouncerate.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void displaytotalcost(){
+     int number = 42; 
+     totalcost.setText(String.valueOf(number));
+  }
+
+  @FXML
+  private void initialize() {
+
+    this.displayImpressions();
+    this.displayUniques();
+    this.displayclicks();
+    this.displaybounces();
+    this.displayconversions();
+    this.displayctr();
+    this.displaycpa();
+    this.displaycpc();
+    this.displaycpm();
+    this.displaybouncerate();
+    this.displaytotalcost();
+      
+  }
+
 
 
 
